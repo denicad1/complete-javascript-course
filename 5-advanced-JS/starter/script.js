babfcd912a21df031cf41fae0ea7fd9e5b689350
the_dude <denicad@msn.com>
2019-06-28 16:52:11 -0400

    i think i might have got it. need to test at home. still need to understand the purpose of closures and first class functions better.
    added comments

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -25,20 +25,34 @@ function pickQuestion(arr){
 //get the answer that the user thinks is right with a prompt
 function getAnswer(){
 let answerString=prompt("Please select the correct answer(just type the number).");
-//since prompt will return a string, we need to parse it to an int
-let answerInt=parseInt(answerString);
-    //return the int so we can use it to check if they are right later
-    return answerInt;
+    return answerString;
 }
 //compare the user's answer with the right answer here and immediately call it
 (function checkAnswer(fn,int){
+    //since prompt will return a string, we need to parse it to an int. can still use string to get out of loop
+    let answerInt=parseInt(int);
     let rightAnswer=fn;
-    //minused 1 so that we can actually make sure the answers will match. check inside the pickQuestion function to understand better what this is.
-    if (int-1===rightAnswer){
-        console.log('yay')
+    //use counter to keep track of score
+    let counter=0;
+    //this is so we can continue to loop the questions until the user wants to end the game
+    let repeat=checkAnswer(pickQuestion(questionsArray)(),getAnswer());
+    
+    return function(){
+        //minused 1 so that we can actually make sure the answers will match. check inside the pickQuestion function to understand better what this is.
+         if (int-1===rightAnswer){
+        console.log('yay');
+        counter++;
+        repeat;
+    }else if(int=="exit"){
+        console.log('game over');
+        console.log(`That was great! you scored ${counter}!`);
     }else{
-        console.log('nay')
+        console.log('nay');
+        repeat;
     }
+    }
+   
+  
 })(pickQuestion(questionsArray)(),getAnswer());
 
 
