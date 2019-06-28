e222b0b97481c0c65bb640b7593778ea0eac538b
Anthony <denicad@msn.com>
2019-06-28 19:31:36 -0400

    still need to work on expert mode

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -7,6 +7,7 @@ let first=new Questions('what year was america created?',[1880,1776,1750],1);
 let second=new Questions('where was the declaration of independence signed?',['Philadelphia','Washington','Delaware'],0);
 let third=new Questions('who wrote the declaration of independence?',['John Wilkes Booth','George Washington','Thomas Jefferson'],2);
 let questionsArray=[first,second,third];
+let counter=0;
 //lets pick a question object at random
 function pickQuestion(arr){
     let rand=Math.floor(Math.random()*3);
@@ -27,30 +28,34 @@ function getAnswer(){
 let answerString=prompt("Please select the correct answer(just type the number).");
     return answerString;
 }
+//this is so I don't have to write out the scores constantly
+function currentScore(score){
+    console.log(`Your Current Score is ${score}`);
+}
+
 //compare the user's answer with the right answer here and immediately call it
 (function checkAnswer(fn,int){
     //since prompt will return a string, we need to parse it to an int. can still use string to get out of loop
-    let answerInt=parseInt(int);
+    let answerInt=parseInt(int)-1;
     let rightAnswer=fn;
-    //use counter to keep track of score
-    let counter=0;
-    //this is so we can continue to loop the questions until the user wants to end the game
-    let repeat=checkAnswer(pickQuestion(questionsArray)(),getAnswer());
-    
-    return function(){
+    // use counter to keep track of score
+            
         //minused 1 so that we can actually make sure the answers will match. check inside the pickQuestion function to understand better what this is.
-         if (int-1===rightAnswer){
+         if (answerInt===rightAnswer){
         console.log('yay');
         counter++;
-        repeat;
+        currentScore(counter);
+         checkAnswer(pickQuestion(questionsArray)(),getAnswer());
     }else if(int=="exit"){
         console.log('game over');
         console.log(`That was great! you scored ${counter}!`);
     }else{
         console.log('nay');
-        repeat;
+        currentScore(counter);
+        checkAnswer(pickQuestion(questionsArray)(),getAnswer()); 
+        
     }
-    }
+    
    
   
 })(pickQuestion(questionsArray)(),getAnswer());
