ba44d1019df0ce1018084f29512dc4f4170ebb0b
the_dude <denicad@msn.com>
2019-07-02 17:00:00 -0400

    started modular design

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,34 @@
+let budgetController= (function(){
+    
+   let x=23;
+    let add=function(a){
+        return x+a;
+    }
+    
+    
+    return{
+        publicTest: function(b){
+            return add(b);
+        }
+    }
+    
+    
+})();
+
+let UIController=(function(){
+    
+    
+    
+})();
+
+let controller=(function(budgetCtrl,UIctrl){
+let z=budgetCtrl.publicTest(5)
+
+
+    return {
+        anotherPublic:function(){
+            console.log(z);
+        }
+    }
+    
+})(budgetController,UIController);
