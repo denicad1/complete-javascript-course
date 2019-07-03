8a51db2b6d4ed5a9e69a4de31d9e7b496221d762
the_dude <denicad@msn.com>
2019-07-03 13:50:15 -0400

    started with eventListeners

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,34 +1,19 @@
+
+//BUDGET CONTROLLER
 let budgetController= (function(){
     
-   let x=23;
-    let add=function(a){
-        return x+a;
-    }
-    
-    
-    return{
-        publicTest: function(b){
-            return add(b);
-        }
-    }
+   
     
     
 })();
-
+//UI CONTROLLER
 let UIController=(function(){
     
     
     
 })();
-
+//GLOBAL APP CONTROLLER
 let controller=(function(budgetCtrl,UIctrl){
-let z=budgetCtrl.publicTest(5)
 
-
-    return {
-        anotherPublic:function(){
-            console.log(z);
-        }
-    }
     
 })(budgetController,UIController);
