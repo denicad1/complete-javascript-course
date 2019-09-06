554b441bba09f2bdc2da3ea543f614ee93908935
the_dude <denicad@msn.com>
2019-09-06 16:25:58 -0400

    starting to get individual recipe fixed for display. hit API call limit for the day I think since async keeps rejecting

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,7 +1,11 @@
 // Global app controller
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
-import { elements, renderLoader, clearLoader } from "./views/base";
+import {
+    elements,
+    renderLoader,
+    clearLoader
+} from "./views/base";
 import Recipe from "./models/Recipe";
 //Global State of the App
 //search object
@@ -12,69 +16,80 @@ const state = {};
 // SEARCH CONTROLLER
 
 const controlSearch = async () => {
-  //1. get the query
-  const query = searchView.getInput(); //TODO
-  if (query) {
-    //2. new search object and add it to state
-    state.search = new Search(query);
-    //3. prepare UI for results
-    searchView.clearInput();
-    searchView.clearResults();
-    renderLoader(elements.searchRes);
+    //1. get the query
+    //    const query = searchView.getInput(); //TODO
+    const query = 'pizza';
+    if (query) {
+        //2. new search object and add it to state
+        state.search = new Search(query);
+        //3. prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
 
-    //4. search for recipe
-    try {
-      await state.search.getResults();
+        //4. search for recipe
+        try {
+            await state.search.getResults();
 
-      //5. render results on UI
-      clearLoader();
-      searchView.renderResults(state.search.result);
-    } catch (err) {
-      alert("something wrong with the search");
-      clearLoader();
+            //5. render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert("something wrong with the search");
+            clearLoader();
+        }
     }
-  }
 };
 
 elements.searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  controlSearch();
+    e.preventDefault();
+    controlSearch();
+});
+
+//TESTING
+window.addEventListener("load", (e) => {
+    e.preventDefault();
+    controlSearch();
 });
 elements.searchResPages.addEventListener("click", (e) => {
-  const btn = e.target.closest(".btn-inline");
-  if (btn) {
-    const goToPage = parseInt(btn.dataset.goto, 10);
-    searchView.clearResults();
-    searchView.renderResults(state.search.result, goToPage);
-  }
+    const btn = e.target.closest(".btn-inline");
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
 });
 
 // RECIPE CONTROLLER
 const controlRecipe = async () => {
-  //get ID from URL
-  const id = window.location.hash.replace("#", "");
-  console.log(id);
-  if (id) {
-    //Prepare the UI for changes
+    //get ID from URL
+    const id = window.location.hash.replace("#", "");
+    console.log(id);
+    if (id) {
+        //Prepare the UI for changes
 
-    //create a new recipe object
-    state.recipe = new Recipe(id);
-    try {
-      //get recipe data
-      await state.recipe.getRecipe();
-      //calculate servings and time
-      state.recipe.calcTime();
-      state.recipe.calcServings();
-      //render recipe
-      //
-      console.log(state.recipe);
-    } catch (err) {
-      alert("Error processing recipe!");
+        //create a new recipe object
+        state.recipe = new Recipe(id);
+
+        //TESTING
+        window.r = state.recipe;
+
+        try {
+            //get recipe data
+            await state.recipe.getRecipe();
+            //calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+            //render recipe
+            //
+            console.log(state.recipe);
+        } catch (err) {
+            alert("Error processing recipe!");
+        }
     }
-  }
 };
 // window.addEventListener("hashchange", controlRecipe);
 // wondow.addEventListener("load", controlRecipe);
 ["hashchange", "load"].forEach((e) => {
-  window.addEventListener(e, controlRecipe);
+    window.addEventListener(e, controlRecipe);
 });
--- 9-forkify/starter/src/js/models/Recipe.js
+++ 9-forkify/starter/src/js/models/Recipe.js
@@ -1,31 +1,87 @@
 import axios from "axios";
-import { key, proxy } from "../config";
+import {
+    key,
+    proxy
+} from "../config";
 export default class Recipe {
-  constructor(id) {
-    this.id = id;
-  }
-  async getRecipe() {
-    try {
-      const res = await axios(
-        `${proxy}https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`
-      );
-      this.title = res.data.recipe.title;
-      this.author = res.data.recipe.publisher;
-      this.img = res.data.recipe.image_url;
-      this.url = res.data.recipe.source_url;
-      this.ingredients = res.data.recipe.ingredients;
-      console.log(res);
-    } catch (error) {
-      alert(error);
+    constructor(id) {
+        this.id = id;
     }
-  }
-  calcTime() {
-    //assuming that we need 15 mins for each 3 ingredients
-    const numIng = this.ingredients.length;
-    const periods = Math.ceil(numIng / 3);
-    this.time = periods * 15;
-  }
-  calcServings() {
-    this.servings = 4;
-  }
+    async getRecipe() {
+        try {
+            const res = await axios(
+                `${proxy}https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`
+            );
+            this.title = res.data.recipe.title;
+            this.author = res.data.recipe.publisher;
+            this.img = res.data.recipe.image_url;
+            this.url = res.data.recipe.source_url;
+            this.ingredients = res.data.recipe.ingredients;
+            console.log(res);
+        } catch (error) {
+            alert(error);
+        }
+    }
+    calcTime() {
+        //assuming that we need 15 mins for each 3 ingredients
+        const numIng = this.ingredients.length;
+        const periods = Math.ceil(numIng / 3);
+        this.time = periods * 15;
+    }
+    calcServings() {
+        this.servings = 4;
+    }
+
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const newIngredients = this.ingredients.map(el => {
+            //1. uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            });
+            //2. remove parentheses
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+            //3. parse ingredients into count, unit and ingredients
+            const arrIng = ingredient.split(' ');
+            const unitIndex = arrIng.findIndex(el2 => {
+                unitsShort.includes(el2);
+            });
+            let objIng;
+            if (unitIndex > -1) {
+                //There is a unit
+                //EX. 4 1/2 cups, arrCount is [4,1/2] -->"4+1/2"
+                //EX. 4 cups, arrCount is [4]
+                const arrCount = arrIng.slice(0, unitIndex);
+                let count;
+                if (arrCount.length === 1) {
+                    count = arrIng[0];
+                } else {
+                    count = eval(arrIng.slice(0, unitIndex).join('+'));
+                }
+
+            } else if (parseInt(arrIng[0], 10)) {
+                //There is NO unit, but the 1st element is a number
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ')
+                }
+
+            } else if (unitIndex == -1) {
+                //There is NO unit and NO number in 1st position
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredient
+                }
+            }
+
+            return objIng;
+
+        });
+        this.ingredients = newIngredients;
+    }
 }
