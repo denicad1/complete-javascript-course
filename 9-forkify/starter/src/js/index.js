0c5436a587334f7cfa53913a9a545adc4693567a
the_dude <denicad@msn.com>
2019-10-03 16:23:35 -0400

    started on changing serving side on button click

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -103,3 +103,17 @@ const controlRecipe = async () => {
 ["hashchange", "load"].forEach((e) => {
     window.addEventListener(e, controlRecipe);
 });
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease , .btn-decrease *')) {
+        //Decrease button is clicked
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+        }
+    } else if (e.target.matches('.btn-increase , .btn-increase *')) {
+        //Increase button is clicked
+        state.recipe.updateServings('inc');
+    }
+    console.log(state.recipe);
+});
