068829101350b8091062796650fe3b872f20c61d
Anthony <denicad@msn.com>
2019-10-07 19:21:25 -0400

    started making shopping list controller

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -2,6 +2,7 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
 import {
     elements,
     renderLoader,
@@ -104,6 +105,19 @@ const controlRecipe = async () => {
 ["hashchange", "load"].forEach((e) => {
     window.addEventListener(e, controlRecipe);
 });
+const controllist = () => {
+    //Create a new list IF there is none yet
+    if (!state.list) state.list = new List();
+
+
+    //Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+
+    });
+
+}
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
@@ -117,6 +131,8 @@ elements.recipe.addEventListener('click', e => {
         //Increase button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controllist();
     }
 });
 
