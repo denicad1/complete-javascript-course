57c90191fabc1b4d9dd15c68de0ea46c5a27b3ce
the_dude <denicad@msn.com>
2019-10-10 12:34:11 -0400

    using second api key

--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -12,7 +12,7 @@ export default class Search {
     async getResults() {
         try {
             const res = await axios(
-                `${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
+                `${proxy}https://www.food2fork.com/api/search?key=${key1}&q=${this.query}`
             );
             this.result = res.data.recipes;
             // console.log(this.result);
