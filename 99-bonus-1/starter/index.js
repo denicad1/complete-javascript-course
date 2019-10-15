4832d13420046ff077a44d4af374987915b2fbd2
Anthony <denicad@msn.com>
2019-10-15 19:20:38 -0400

    still working with node

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const http = require('http');
-
+const url = require('url');
 
 
 
@@ -11,10 +11,29 @@ const laptopData = JSON.parse(json);
 
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-type': 'text/html'
-    });
-    res.end(`This is the response!`);
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+    console.log(url.parse(req.url, true));
+    //console.log(pathName);
+    if (pathName === '/products' || pathName === '/') {
+
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+        res.end(`This is the products page`);
+    } else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+        res.end(`this is the laptop page ${id}`);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+        res.end(`page not found`);
+    }
+
+
 });
 
 server.listen(1337, `127.0.0.1`, () => {
