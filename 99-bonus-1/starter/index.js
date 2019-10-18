a99bf80cc9c901ad1303a4fcf7e25c659aa8ad20
the_dude <denicad@msn.com>
2019-10-18 16:09:44 -0400

    created function to replace keywords in templates

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -50,3 +50,16 @@ const server = http.createServer((req, res) => {
 server.listen(1337, `127.0.0.1`, () => {
     console.log(`listening for requests now`);
 });
+
+function replaceTemplate(originalHTML, laptop) {
+    let output = originalHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+}
