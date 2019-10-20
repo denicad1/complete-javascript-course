3206964bdbdbf7ab1007eefa6b84ad76e4d807c4
Anthony <denicad@msn.com>
2019-10-20 12:17:53 -0400

    finished templates and got pictures

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -12,25 +12,52 @@ const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname;
-    const id = url.parse(req.url, true).query.id;
-    console.log(url.parse(req.url, true));
     //console.log(pathName);
+    const id = url.parse(req.url, true).query.id;
+    //console.log(url.parse(req.url, true));
+    //console.log(pathName);
+
+
+    //PRODUCT OVERVIEW
     if (pathName === '/products' || pathName === '/') {
 
         res.writeHead(200, {
             'Content-type': 'text/html'
         });
-        res.end(`This is the products page`);
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
+            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+                res.end(overviewOutput);
+            });
+        });
+
+        //LAPTOP DETAIL
     } else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, {
             'Content-type': 'text/html'
         });
         fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
             const laptop = laptopData[id];
-
+            const output = replaceTemplate(data, laptop);
             res.end(output);
         });
-    } else {
+        //IMAGES
+    } else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img/${pathName}`, (err, data) => {
+            res.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            res.end(data);
+
+
+        })
+
+
+    }
+    //URL NOT FOUND
+    else {
         res.writeHead(404, {
             'Content-type': 'text/html'
         });
